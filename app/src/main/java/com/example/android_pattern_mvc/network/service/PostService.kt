a39831648d2ce8c6bfa35a68769cd91f1a4c7b6b
ShodiyorXurramov <xurramoshodiyor377@gmail.com>
2022-04-25 12:02:30 +0500

    Initial commit

---
+++ app/src/main/java/com/example/android_pattern_mvc/network/service/PostService.kt
@@ -0,0 +1,20 @@
+package com.example.android_pattern_mvc.network.service
+
+
+import com.example.android_pattern_mvc.model.Post
+import retrofit2.Call
+import retrofit2.http.*
+
+interface PostService {
+
+    @Headers(
+        "Content-type:application/json"
+    )
+
+    @GET("posts")
+    fun listPost(): Call<ArrayList<Post>>
+
+    @DELETE("posts/{id}")
+    fun deletePost(@Path("id") id: Int): Call<Post>
+
+}
